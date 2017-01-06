f1b7692958a00d87635dce024b3dce123be39a67
Fedor Lyanguzov <fedor.lyanguzov@ya.ru>
2017-01-06 03:58:56 +0300

    Working on drag and drop in analyzes

--- allergens/analyzes/views.py
+++ allergens/analyzes/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from patients.models import Patient
+
+plate = {'horizontal_indices': ['A','B','C','D','E','F','G','H'], 
+         'vertical_indices': [1,2,3,4,5,6,7,8,9,10,11,12]}
 
 def analyzes(request):
-    return render(request, 'analyzes/dashboard.html')
+    context = {'patients': Patient.objects.all(), 'plate':plate }
+    return render(request, 'analyzes/dashboard2.html', context)
